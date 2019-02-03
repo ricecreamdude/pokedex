1047a894583b52ee84cde42e8e685fe2f169129c
Joshua Ho <ho.joshua4@gmail.com>
2019-02-02 22:41:36 -0800

    Adding boilerplate

---
+++ app/containers/FeaturePage/ListItemTitle.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const ListItemTitle = styled.p`
+  font-weight: bolder;
+`;
+
+export default ListItemTitle;
---
+++ app/containers/PokedexPage/messages.js
@@ -0,0 +1,15 @@
+/*
+ * NotFoundPage Messages
+ *
+ * This contains all the text for the NotFoundPage component.
+ */
+import { defineMessages } from 'react-intl';
+
+export const scope = 'boilerplate.containers.PokedexPage';
+
+export default defineMessages({
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'Welcome to the Pokedex.',
+  },
+});
